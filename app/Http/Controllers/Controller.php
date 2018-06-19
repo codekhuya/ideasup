c515d15a400fd1bf50d9c6a0c80ef31acb7992c4
Thanh Huynh <codekhuya@gmail.com>
2018-06-19 15:44:17 +0700

    Thêm hàm gửi data và message để dùng chung

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,7 +7,29 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
+use Illuminate\Http\Request;
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function sendResponse($result, $message){
+        $response = [
+            'success' => true,
+            'message' => $message,
+            'data'    => $result,
+        ];
+        return response()->json($response, 200);
+    }
+
+    public function sendError($error, $errorMassage = [], $code = 404){
+        $response = [
+            'success' => false,
+            'message' => $error,
+        ];
+        if(!empty($errorMassage)){
+            $response['data'] = $errorMassage;
+        }
+        return response()->json($response, $code);
+    }
 }
